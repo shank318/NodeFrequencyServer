394f7cce310a4c6292721b31b1c3a647ff6a0617
shashank <shank@shashanks-MacBook-Pro.local>
2015-01-08 11:58:24 +0530

    pusing

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var bodyParser = require('body-parser');
 
 
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 
 
 app.post('/api/record', function(req, res) {
@@ -25,43 +25,11 @@ app.get('/api/status', function(req, res) {
 });
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-
 app.listen('1337', function() {
     console.log('Express server listening on port ' + '1337');
 
 });
+
 
 
 
